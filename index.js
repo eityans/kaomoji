61a4a7f07d20097f4bd0a14aa6f9a9c486e60ad1
eityans <eityans@gmail.com>
2019-11-18 15:27:12 +0000

    merge

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // モジュールのインポート
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+const dialogflow = require("dialogflow");
 
 // -----------------------------------------------------------------------------
 // パラメータ設定
@@ -18,6 +19,15 @@ server.listen(process.env.PORT || 3000);
 
 // APIコールのためのクライアントインスタンスを作成
 const bot = new line.Client(line_config);
+
+// Dialogflowのクライアントインスタンスを作成
+const session_client = new dialogflow.SessionsClient({
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    }
+});
 
 // -----------------------------------------------------------------------------
 // ルーター設定
@@ -40,6 +50,43 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                     text: "(っ＾ω＾ｃ)"
                 }));
             }
+            if (event.message.text == "急性胃腸炎"){
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "_:(´ཀ`」 ∠):_"
+                }));
+            }
+
+            events_processed.push(
+                session_client.detectIntent({
+                    session: session_client.sessionPath(process.env.GOOGLE_PROJECT_ID, event.source.userId),
+                    queryInput: {
+                        text: {
+                            text: event.message.text,
+                            languageCode: "ja",
+                        }
+                    }
+                }).then((responses) => {
+                    if (responses[0].queryResult && responses[0].queryResult.action == "emotion"){
+                        let message_text
+                        message_text = "(っ＾ω＾ｃ)♪"
+                        /*
+                        if (responses[0].queryResult.parameters.fields.menu.stringValue){
+                            message_text = `毎度！${responses[0].queryResult.parameters.fields.menu.stringValue}ね。どちらにお届けしましょ？`;
+                        } else {
+                            message_text = `毎度！ご注文は？`;
+                        }
+                        */
+                        return bot.replyMessage(event.replyToken, {
+                            type: "text",
+                            text: message_text
+                        });
+                    }
+                })
+            );
+
+
         }
     });
 
