79e0e4b3cd63e4ff9dfa6561d40ec07e6754b57d
eityans <eityans@gmail.com>
2019-11-23 11:24:52 +0000

    fix

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 exports.pool = pg.Pool ({
   host: process.env.ENV_HOST,
-  databese: process.env.ENV_DB,
+  databese: process.env.ENV_DATABASE,
   user: process.env.ENV_USER,
   port: 5432,
   password: process.env.ENV_PASSWORD,
