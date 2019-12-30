4397e14ee106b873f084a04ae04e6a38cf40bd84
eityans <eityans@gmail.com>
2019-12-30 12:02:42 +0900

    test

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
-                    text: "٩( 'ω' )و"
+                    text: "(っ＾ω＾ｃ)"
                 }));
             }
 
